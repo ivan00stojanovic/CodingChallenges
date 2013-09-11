2899da007a4420b36b4c21e2f362d38dda03a239
Marko Ilic <ivanstojanovic00@gmail.com>
2013-09-11 19:18:25 -0400

    solution

---
+++ 6kyu/Sort the Odd.js
@@ -0,0 +1,37 @@
+/* You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+Examples
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0] */
+
+function sortArray(array) {
+ let  counter = 0
+let sortedOdd = []
+
+for(let i = 0;i<array.length;i++){
+
+    if(array[i]  % 2 !== 0){
+
+        sortedOdd.push(array[i])
+
+        array[i] = '_'
+
+    }
+
+}
+
+sortedOdd.sort(function(a,b){return a - b})
+
+for(let j = 0; j<array.length; j++){
+
+    if(array[j] === '_'){
+
+      array[j] = sortedOdd[counter]
+
+      counter++
+
+    }
+}
+   return array
+}
