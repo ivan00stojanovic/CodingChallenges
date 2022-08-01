f064e9e89e2534db94a378c2c2589a02e6429163
Marko Ilic <ivanstojanovic00@gmail.com>
2022-08-01 18:49:59 -0400

    solution

---
+++ If you can't sleep, just count sheep!!.js
@@ -0,0 +1,14 @@
+/* var countSheep = function (num){
+    let sheepCount = ''
+  for(let i = 1; i<= num; i++){
+     sheepCount += i + ' sheep...'
+  }
+    return sheepCount */
+
+
+let  countSheep = function (num){
+    let sheepCount = ''
+  for(let i = 1; i<= num; i++){
+     sheepCount += i + ' sheep...'
+  }
+    return sheepCount
