46047068e14295f88923d4cbd84c412a63f39173
Marko Ilic <ivanstojanovic00@gmail.com>
2022-08-27 11:20:32 -0400

    solution

--- Grasshopper Messi Goals.js
+++ Grasshopper Messi Goals.js
@@ -18,4 +18,4 @@ var laLigaGoals = 43
 var championsLeagueGoals = 10
 var copaDelReyGoals = 5
 
-var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals`'
