194b6c0101af0e2087376f967f9380478b398c4c
Marko Ilic <ivanstojanovic00@gmail.com>
2022-08-28 18:48:55 -0400

    solution

--- What's the real floor.js
+++ What's the real floor.js
@@ -3,7 +3,7 @@
 Write a function that given a floor in the american system returns the floor in the european system.
 
 With the 1st floor being replaced by the ground floor and the 13th floor being removed, the numbers move down to take their place. 
-In case of above 13, they move down by two because there are two omitted numbers below them.
+~In case of above 13, they move down by two because there are two omitted numbers below them.
 
 Basements (negatives) stay the same as the universal level.
 
