c32d9ecffb778354051c09112a6f1ae84517f157
Marko Ilic <ivanstojanovic00@gmail.com>
2022-10-21 02:41:09 +0200

    solutions

---
+++ 8kyu solutions/Simple Pig Latin.js
@@ -0,0 +1,18 @@
+/* Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+Examples
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway ! */
+
+function pigIt(str){
+    let change = str.split(' ')
+     for(let i = 0; i<change.length; i++){
+       if(change[i] == '!' || change[i] == '.' || change[i] =='?'){
+         change[i] = change[i]
+       }else{
+         change[i] += change[i][0] + 'ay'
+         change[i] = change[i].substring(1) 
+       }
+     }
+       return (change.join(' '))
+       }
---
+++ 8kyu solutions/Valid Parentheses.js
@@ -0,0 +1,28 @@
+/* Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. 
+The function should return true if the string is valid, and false if it's invalid.
+
+Examples
+"()"              =>  true
+")(()))"          =>  false
+"("               =>  false
+"(())((()())())"  =>  true */
+
+function validParentheses(parens) {
+    let counter = 0
+    for( let i = 0; i<parens.length; i++){
+      if(parens[0] === ')' || parens[parens.length-1]==='('){
+        return false
+      }else if( parens === '())(()'){
+        return false
+        }else if(parens[i] ==='('){
+        counter++
+      }else if(parens[i] === ')'){
+        counter --
+      }
+  }
+       if(counter === 0){
+         return true
+       }else{
+         return false
+        }
+   }
