30986e354de6661333daa01433aa63adacc8793f
Marko Ilic <ivanstojanovic00@gmail.com>
2022-11-08 22:59:26 +0100

    added solutions to new folder

---
+++ 8kyu solutions/If you can't sleep, just count sheep!!.js
@@ -0,0 +1,15 @@
+/* var countSheep = function (num){
+    let sheepCount = ''
+  for(let i = 1; i<= num; i++){
+     sheepCount += i + ' sheep...'
+  }
+    return sheepCount */
+
+
+let  countSheep = function (num){
+    let sheepCount = ''
+  for(let i = 1; i<= num; i++){
+     sheepCount += i + ' sheep...'
+  }
+    return sheepCount
+``
