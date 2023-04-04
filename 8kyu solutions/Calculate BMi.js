555c92e3d92d5332efc1ba592cc082bd3a52edf5
Marko Ilic <ivanstojanovic00@gmail.com>
2023-04-04 15:15:50 +0200

    solution

--- 8kyu solutions/Calculate BMi.js
+++ 8kyu solutions/Calculate BMi.js
@@ -6,7 +6,8 @@ if bmi <= 25.0 return "Normal"
 
 if bmi <= 30.0 return "Overweight"
 
-if bmi > 30 return "Obese"*/
+if bmi > 30 return "Obese"
+Good luck :)*/
 
 function bmi(weight, height) {
     let bmi = weight / (height * height);
