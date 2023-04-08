dae86314e7dfe37b3bff945cd31e0877b61171ba
Marko Ilic <ivanstojanovic00@gmail.com>
2023-04-08 12:43:26 +0200

    solution

--- 8kyu solutions/Remove duplicates from list.js
+++ 8kyu solutions/Remove duplicates from list.js
@@ -1,6 +1,6 @@
 /* Define a function that removes duplicates from an array of numbers and returns it as a result.
 
-The order of the sequence has to stay the same. */
+The order of the sequence has to stay the same.*/
 
 distinct = (a) => {
     return [...new Set(a)];
