f6764c2fc51173031a1dc75577331254c9720bbf
Ivan Stojanovic <ivanstojanovic00@gmail.com>
2023-08-16 13:08:05 -0400

    file restructuring

---
+++ LeetCode/Easies/Best Time to Buy and Sell Stocks.js
@@ -0,0 +1,58 @@
+/* You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+  */
+
+// /**
+//  * @param {number[]} prices
+//  * @return {number}
+//  */
+// var maxProfit = function(prices) {
+//    let maxDiff = 0
+//   let sum 
+// for(let i = 0; i < prices.length; i++){
+//   for(let j = i + 1; j < prices.length; j++){
+//     sum = prices[j] - prices[i]
+//       if(sum > maxDiff) maxDiff = sum
+//   }
+// }
+//     return maxDiff
+// };
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minPrice = prices[0];
+    let maxProfit = 0;
+  
+    for (let i = 1; i < prices.length; i++) {
+      if (prices[i] < minPrice) {
+        minPrice = prices[i];
+      } else {
+        const potentialProfit = prices[i] - minPrice;
+        if (potentialProfit > maxProfit) {
+          maxProfit = potentialProfit;
+        }
+      }
+    }
+  
+    return maxProfit;
+  };
