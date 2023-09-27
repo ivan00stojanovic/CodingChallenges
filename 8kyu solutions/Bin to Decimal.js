753116c3b4de9672c49f9aad1e5c1adf48761c0e
Ivan Stojanovic <ivanstojanovic00@gmail.com>
2023-09-27 01:54:27 -0400

    solution

---
+++ 8kyu solutions/Bin to Decimal.js
@@ -0,0 +1,7 @@
+/* Complete the function which converts a binary number (given as a string) to a decimal number. */
+
+function binToDec(binaryString) {
+    // Use parseInt with base 2 to convert the binary string to a decimal number
+    return parseInt(binaryString, 2);
+  }
+  
