0d8d185fb5512e3dcce905d8303ed6859c0d804c
Ivan Stojanovic <ivanstojanovic00@gmail.com>
2024-01-16 00:23:00 +0100

    second solution

--- 8kyu solutions/Basic Subclasses - Adam and Eve.js
+++ 8kyu solutions/Basic Subclasses - Adam and Eve.js
@@ -28,3 +28,17 @@ class God {
       super();
     }
   }
+
+//   second solution
+
+class God{
+    /**
+     * @returns Human[]
+     */
+      static create(){
+        return [new Man, new Woman];
+      }
+    }
+    class Human{}
+    class Man extends Human{}
+    class Woman extends Human{}
