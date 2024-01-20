c1dc5c89cb0250ee509843d36752a0b8950955e3
Ivan Stojanovic <ivanstojanovic00@gmail.com>
2024-01-20 15:16:40 +0100

    challenge 2

--- Interview challenges/Palindrome Checker.js
+++ Interview challenges/Palindrome Checker.js
@@ -1,4 +1,6 @@
-/* Make a function which checks if a string argument is a palindrome. Examples: 
+/* IT Academy Test 1 
+
+2. Make a function which checks if a string argument is a palindrome. Examples: 
     anavolimilovana je palindrom
     sirimamiris je palindrom
     beograd nije palindrom */
@@ -6,3 +8,7 @@
     function palindromeChecker(string){
         return string === string.split('').reverse().join('')
       }
+
+    console.log(palindromeChecker('ivan'))
+    console.log(palindromeChecker('anavolimilovana'))
+    console.log(palindromeChecker('leonnoel'))
