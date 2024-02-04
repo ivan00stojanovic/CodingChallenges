9afdcc3d57d460cf5552319c30471af0f8b2b218
Ivan Stojanovic <ivanstojanovic00@gmail.com>
2024-02-04 00:02:47 -0500

    solution

--- 8kyu solutions/Template Strings.js
+++ 8kyu solutions/Template Strings.js
@@ -5,9 +5,8 @@ Your task is to return the correct string using the Template String Feature.
 Input
 Two Strings, no validation is needed.
 Output
-You must output a string containing the two strings with the word ```' are '```
-Reference: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/template_strings */
+You must output a string containing the two strings with the word ```' are '``` */
 
-var templateStrings = function(noun, adjective) {
-    return `${noun} are ${adjective}`
-  }
+
+
+  const templateStrings = (noun, adjective) =>  {return `${noun} are ${adjective}`}
