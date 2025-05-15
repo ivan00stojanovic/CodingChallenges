7fdef08851926be473fc51842e7994337600a606
Ivan Stojanovic <ivanstojanovic00@gmail.com>
2025-05-15 17:05:41 +0200

    fixed solution

--- LeetCode/Missing Number.js
+++ LeetCode/Missing Number.js
@@ -18,9 +18,19 @@ Input: nums = [9,6,4,2,3,5,7,0,1]
 Output: 8
 Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums. */
 
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var missingNumber = function(nums) {
-    nums.sort()
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i + 1] !== nums[i] + 1) return i + 1
-    }
+    let sumWithout = nums.reduce((acc, cum) => acc + cum, 0)
+    let sum = 0 
+    let counter = nums.length
+       while (counter > 0){
+        sum += counter
+        counter--
+       }
+        return sum - sumWithout
 };
