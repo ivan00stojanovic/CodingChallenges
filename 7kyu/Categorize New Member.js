6cb4b3ae10c853d908fa98b9c90bd17f661520b9
Ivan Stojanovic <ivanstojanovic00@gmail.com>
2025-07-10 01:17:08 -0400

    refactored solution

--- 7kyu/Categorize New Member.js
+++ 7kyu/Categorize New Member.js
@@ -27,3 +27,14 @@ function openOrSenior(data){
   }
       return result
 }
+
+// ternary operator refactor
+
+function openOrSenior(data){
+  let result = []
+  for(let i = 0; i < data.length; i++){
+    data[i][0] >=55 && data[i][1] > 7 ? result.push('Senior') 
+    :                                   result.push('Open')
+}
+      return result
+    }
